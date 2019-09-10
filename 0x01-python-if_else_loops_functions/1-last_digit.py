afafe20f6d0053056c2f3bf27fbf520a4a224d8d
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-09-10 22:28:56 +0000

    modulo negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 1:
-   number =  number * -1
-   last = number % 10
+if number < 0:
+    last = (number % 10) * -1
 else:
-   last = number % 10
+    last = number % 10
 if last > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last))
+    print("Last digit of", number, "is", last,
+          "and is greater than 5")
 elif last == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, last))
 elif last < 6 and last != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last))
+    print("Last digit of", number, "is", last,
+          "and is less than 6 and not 0")
