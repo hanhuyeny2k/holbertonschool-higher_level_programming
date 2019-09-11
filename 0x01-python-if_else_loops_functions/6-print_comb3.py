4f7923cc622f2b40a8d7615741ebfc7135d0ff09
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-09-11 21:26:53 +0000

    edit end=

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,7 @@ for i in range(0, 10):
         if i < j:
             print("{:d}".format(i), end="")
             if i == 8 and j == 9:
-                print("{:d}".format(j), end="")
+                print("{:d}".format(j))
             else:
                 print("{:d}".format(j), end=", ")
+
