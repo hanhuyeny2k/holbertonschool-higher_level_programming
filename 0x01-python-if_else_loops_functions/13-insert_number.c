50455f37606b41bbbb0a7c3dc42ad51fdeea5afb
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-09-12 00:02:12 +0000

    add node in middle linkedlist

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *ptr;
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+	ptr = *head;
+	if (ptr == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	for (; ptr->next != NULL && number > ptr->next->n; ptr = ptr->next)
+		;
+	new->next = ptr->next;
+	ptr->next = new;
+	return (new);
+}
