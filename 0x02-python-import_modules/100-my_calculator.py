e46b1cdc2a8262ba266a90ff4c728f7039af0b22
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-09-13 05:29:41 +0000

    calculator replica

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+if __name__ == "__main__":
+    argc = len(sys.argv)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if argc != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif sys.argv[2] not in '+-*/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    elif sys.argv[2] == '+':
+        to_add = add(int(sys.argv[1]), int(sys.argv[3]))
+        print("{:d} + {:d} = {:d}".format(a, b, to_add))
+    elif sys.argv[2] == '-':
+        to_sub = add(int(sys.argv[1]), int(sys.argv[3]))
+        print("{:d} - {:d} = {:d}".format(int(a, b, to_sub))
+    elif sys.argv[2] == '*':
+        to_mul =add(int(sys.argv[1]), int(sys.argv[3]))
+        print("{:d} * {:d} = {:d}".format(a, b, to_mul))
+    elif sys.argv[2] == '/':
+        to_div = add(int(sys.argv[1]), int(sys.argv[3]))
+        print("{:d} / {:d} = {:d}".format(a, b, to_div))
