22f7787a04208bc04ee499a4ae6f34bf5be1141f
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-09-18 02:15:54 +0000

    remove c and C from a string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ''
+    for a in my_string:
+        if a in "cC":
+            new_string += ''
+        else:
+            new_string += a
+    return new_string
+
