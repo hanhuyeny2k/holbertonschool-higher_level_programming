106c65ca2440d2447c6e3c61d8afd83b83e96c26
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-09-20 03:07:33 +0000

    add the edge case

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is not None and a_dictionary is not {}:
         return max(a_dictionary.keys(), key=a_dictionary.get)
     else:
         return None
