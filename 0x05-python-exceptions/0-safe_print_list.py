9c8636b2f0b25d1883ed83a3e4cb2ebc73852663
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-01 03:38:05 +0000

    print the number of elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    for e in range (x):
+        try:
+            print(format(my_list[e]), end="")
+            counter += 1
+        except:
+            break
+    print()
+    return counter
