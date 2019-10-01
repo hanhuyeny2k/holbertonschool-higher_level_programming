7a55b72fe40545aecb54fe83fd3d78fafeedf022
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-01 03:39:43 +0000

    fixed pep8 format

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     counter = 0
-    for e in range (x):
+    for e in range(x):
         try:
             print(format(my_list[e]), end="")
             counter += 1
