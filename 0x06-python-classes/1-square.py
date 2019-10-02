85c47533e981305f1c649416788ea635bb910c1d
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-02 01:08:21 +0000

    Corrected PEP8 error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines the size of the square"""
+
+
 class Square:
     """Represents a square, creates a private instance attribute: size."""
     def __init__(self, size):
