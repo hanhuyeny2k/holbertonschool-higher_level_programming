98f64608cddafe0f129485365d2cc09fd83c23cc
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-02 03:03:10 +0000

    Print a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Defines the size of the square."""
+
+
+class Square:
+    """Represents a square, creates a private instance attribute: size."""
+    @property
+    def size(self):
+        """Create a getter to protect and reserve the data."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Create a setter to protect putting bad data inside a square."""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __init__(self, size=0):
+        """Initializing the size of the square."""
+        self.__size = size
+
+    def area(self):
+        """Return the area of a square."""
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        """Print a square."""
+        if self.__size == 0:
+            print()
+        else:
+            for e in range(0, self.__size):
+                for f in range(0, self.__size):
+                    print("#", end="")
+                print()
