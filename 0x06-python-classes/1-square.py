e60def6fab4b71632596fce71f787c8bd5b17124
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-02 01:04:03 +0000

    Added docstring for module and function

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Defines the size of the square"""
 Class Square:
     """Represents a square, creates a private instance attribute: size."""
     def __init__(self, size):
+        """Initializing the size of the square"""
         self.__size = size
