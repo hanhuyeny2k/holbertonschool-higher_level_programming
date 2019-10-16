9541e70f78c2fc8e525e63e64b5b24c899e1c526
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-16 04:03:16 +0000

    Rectangle inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Defines a Rectangle using BaseGeometry module"""
+
+
+class Rectangle(BaseGeometry):
+    """Represent a Rectangle."""
+    def __init__(self, width, height):
+        """Initializing the width and height."""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("heigth", height)
