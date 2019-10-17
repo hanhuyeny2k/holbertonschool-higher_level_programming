f4be549741342affea8c59175ae414c5c2430369
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-17 03:17:09 +0000

    To JSON string

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+"""
+Use JSON module to returns the JSON representation of an object (string)
+
+"""
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
