22f7fbb55b19f51bc342f51002354f70fdfd29c5
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-17 03:15:51 +0000

    Read n lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,24 @@
+#!/bin/usr/python3
+"""
+Using def read_lines(filename="", nb_lines=0)
+
+to reads n lines of a text file (UTF8) and prints it to stdout
+
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """Reads n lines of a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as myFile:
+        if nb_lines <= 0 or nb_lines >= len(myFile.readlines()):
+            for line in myFile:
+                print(line, end="")
+        else:
+            counter = 0
+            myFile.seek(0)
+            for line in myFile:
+                if counter == nb_lines:
+                    break
+                else:
+                    print(line, end="")
+                counter += 1
