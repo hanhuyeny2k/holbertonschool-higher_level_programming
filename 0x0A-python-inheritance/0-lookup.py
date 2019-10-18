c43a5ccd9c478a96768d3d380e26826c5a27e186
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-18 05:06:58 +0000

    Changed the parameter

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,4 +7,4 @@ Return the list of available attributes and methods of an object
 
 def lookup(obj):
     """Search for available attributes and methods of an object"""
-    return dir(list)
+    return dir(obj)
