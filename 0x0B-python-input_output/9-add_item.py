b9ada702f9d6ca6455a528fbef0d1e31cb4668be
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-10-18 02:38:15 +0000

    Added ValueError

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,10 +9,10 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
-if__name__ == "__main__":
+if __name__ == "__main__":
     try:
-        myList = load_from_json_file(add_item.json)
-    except FileNotFoundError:
+        myList = load_from_json_file("add_item.json")
+    except (FileNotFoundError, ValueError):
         myList = []
     myList += sys.argv[1:]
-    save_to_json_file(myList, add_item.json)
+    save_to_json_file(myList, "add_item.json")
