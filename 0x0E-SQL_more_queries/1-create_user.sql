e02775b60ce64b8478946cdddd755944cff0506d
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-11-26 23:40:43 +0000

    Create a new user with grant all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create the MySQL server user with a password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANTS ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
