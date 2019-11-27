5e1f972b529284a17b20359ca4fc9b0ae82ded63
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-11-27 02:20:31 +0000

    Add WHERE show.id

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,5 @@
 -- Import the given data dump then list all shows contain in that database that have at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id 
+	FROM tv_shows, tv_show_genres 
+	WHERE tv_shows.id = tv_show_genres.show_id 
+	ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
