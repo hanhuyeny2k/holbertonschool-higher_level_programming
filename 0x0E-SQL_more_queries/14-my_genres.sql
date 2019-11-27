7ffc95064131d4b4dd88857a62595b2beb609aee
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-11-27 21:42:34 +0000

    list all genres of the show Dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,4 @@
+-- Use the database to list all genres of the show Dexter
+SELECT tv_genres.name FROM tv_shows, tv_show_genres, tv_genres
+WHERE tv_shows.title = 'Dexter' AND tv_shows.id = tv_show_genres.show_id AND tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_genres.name ASC;
