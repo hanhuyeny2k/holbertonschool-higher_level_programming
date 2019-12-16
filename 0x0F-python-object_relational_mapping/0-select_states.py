28eac6a29bf8bf259e754140c4f5280e067c312c
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-12-16 18:54:23 +0000

    close curr and db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -17,3 +17,5 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     for row in cur.fetchall():
         print(row[0], ",", row[1])
+    db.close()
+    cur.close()
