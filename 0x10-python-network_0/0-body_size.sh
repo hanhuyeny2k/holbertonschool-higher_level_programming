c8d12b45d51659b7f47e9edf1ec5691afac0165b
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-01-07 03:25:05 +0000

    fixed commentation

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes the URL isplays the size of the body of the response
+# Send a request to a URL and display the size of the body of the response
 curl "$1" -I -s | grep Content-Length | cut -d' ' -f2
