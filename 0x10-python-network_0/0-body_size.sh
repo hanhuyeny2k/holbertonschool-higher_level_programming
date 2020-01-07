9ff621635d0dd35998c648b3dd2276ab37c84185
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-01-07 03:22:11 +0000

    removed -Fi from grep

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL and displays the size of the body
-# of the response
-curl "$1" -I -s | grep -Fi Content-Length | cut -d' ' -f2
+# Takes the URL isplays the size of the body of the response
+curl "$1" -I -s | grep Content-Length | cut -d' ' -f2
