4b786a6e3f1205aa5ba29b1c406c3d16d4b3a87c
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-01-07 03:16:35 +0000

    fixed the space

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL and displays the size of the body
 # of the response
-curl "$1" -I -s | grep -Fi Content-Length | cut -d ' ' -f2
+curl "$1" -I -s | grep -Fi Content-Length | cut -d' ' -f2
