bd97923e2330c2c4076981b1c037a7b11b98102d
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-02-04 22:41:25 +0000

    fixed semistandard error

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myVar = process.argv.slice(2) 
+const myVar = process.argv.slice(2);
 if (myVar.length >= 2) {
   console.log('Argument found');
 } else if (myVar.length === 1) {
