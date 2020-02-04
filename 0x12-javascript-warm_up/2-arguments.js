c5c6ab7f86bed40d7200c0950a68520276c66efa
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-02-04 22:26:58 +0000

    process.argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myVar = process.argv.slice(2) 
+if (myVar.length >= 2) {
+  console.log('Argument found');
+} else if (myVar.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
