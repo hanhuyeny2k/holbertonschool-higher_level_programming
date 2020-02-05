e59ef0f6c90e909d40c1c5215ec5955a6df80448
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-02-05 07:31:01 +0000

    fixed the for loop

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 const myVar = Number.parseInt(process.argv[2]);
-const i = 0, j = 0;
 if (isNaN(myVar)) {
   console.log('Missing size');
 } else {
+  let row = '';
   for (let i = 0; i < myVar; i++) {
-    for (let j = 0; j < myVar; j++) {
-	  console.log('#');
-    }
+    row += '#';
+  }
+  for (let i = 0; i < myVar; i++) {
+    console.log(row);
   }
 }
