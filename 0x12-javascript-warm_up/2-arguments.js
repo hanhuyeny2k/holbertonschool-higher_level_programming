2b0bd5f9eb6757bfb9221ae2dd7de76a8482bad0
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-02-05 03:22:57 +0000

    capitalized typo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myVar = process.argv.slice(2);
 if (myVar.length === 0) {
-  console.log('No Argument');
+  console.log('No argument');
 } else if (myVar.length === 1) {
   console.log('Argument found');
 } else {
