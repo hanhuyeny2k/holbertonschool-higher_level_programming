cad2ed50320b29a8240df4bdfcdb2db63355b85d
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-02-11 02:02:03 +0000

    testing

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+  print() {
+    let row = '';
+    for (let i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+module.exports = Rectangle;
