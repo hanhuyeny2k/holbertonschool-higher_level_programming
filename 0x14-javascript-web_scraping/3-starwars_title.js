6898c944e20296629f97180bbad683de50ff2bc0
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-02-12 04:35:05 +0000

    took out the else

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,6 @@ const url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, { json: true }, function (error, response, body) {
   if (error) {
     console.log(error);
-  } else {
-    console.log(body.title);
   }
+  console.log(body.title);
 });
