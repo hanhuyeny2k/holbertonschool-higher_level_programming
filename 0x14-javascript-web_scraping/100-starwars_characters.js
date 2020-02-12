2c835f7b5b66a3481c3e48a56a94c310aa318878
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-02-12 20:27:21 +0000

    testing

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+
+request(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(body.title);
+});
